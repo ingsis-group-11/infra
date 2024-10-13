55f648fdc0488c8d4ef701d84889fecfa37b2de9
TomasSerra <tomi.serra@gmail.com>
2024-10-13 18:07:49 -0300

    Pull images working

--- pull_images.sh
+++ pull_images.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
+source ~/.bashrc
+
 GHCR_USER="TomasSerra"
 
 GHCR_PAT="${GHCR_PAT}"
 
 if [ -z "$GHCR_PAT" ]; then
-  echo "Error: La variable de entorno GHCR_PAT no está definida."
+  echo "Error: The GHCR_PAT environment variable is required."
   exit 1
 fi
 
-echo "Autenticando en ghcr.io..."
+echo "Autenticating in ghcr.io..."
 echo "$GHCR_PAT" | docker login ghcr.io -u "$GHCR_USER" --password-stdin
 
 if [ $? -ne 0 ]; then
-  echo "Error: No se pudo autenticar en GitHub Container Registry."
+  echo "Error: Can not authenticate in ghcr.io."
   exit 1
 fi
 
-echo "Haciendo docker-compose pull..."
-docker compose pull snippet-manager permissions-manager printscript-service
+echo "Docker-compose pull..."
+docker compose pull snippet-manager permission-manager printscript-service
 
 if [ $? -ne 0 ]; then
-  echo "Error: No se pudieron traer las imágenes."
+  echo "Error: Can not pull images."
   exit 1
 fi
-
-echo "Iniciando servicios con docker-compose up..."
-docker compose up
